74767bd63d8a7b17e0f98e132194dbb7247d5db3
Ravi Goswami <zneroravi@gmail.com>
2018-02-25 13:06:06 +0530

    home file

---
+++ index.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Home - SHRIJI BABA SARASWATI VIDYA MANDIR</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <!-- Simple Line Font -->
+    <link rel="stylesheet" href="css/simple-line-icons.css">
+    <!-- Slider / Carousel -->
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/slick-theme.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <!-- Main CSS -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!--============================= HEADER =============================-->
+    <div class="header-topbar">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-6 col-sm-8 col-md-9">
+                    <div class="header-top_address"> 
+                        <div class="header-top_list">
+                            <span class="icon-phone"></span>0565 2422021
+                        </div>
+                        <div class="header-top_list">
+                            <span class="icon-envelope-open"></span>sjb_vidyamandir@yahoo.com
+                        </div>
+                        <div class="header-top_list">
+                            <span class="icon-location-pin"></span>Govardhan Road, Mathura
+                        </div>
+                    </div>
+                    <div class="header-top_login2">
+                        <a href="#">Login </a>/ 
+                        <a href="#">Sign Up </a>
+                    </div>
+                </div>
+                <div class="col-xs-6 col-sm-4 col-md-3">
+                    <div class="header-top_login mr-sm-3">
+                        <a href="login.php">Login </a>/ 
+                        <a href="coming-soon.html"> Sign Up </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php 
+    include 'inc/menu.html';
+    ?>
+</nav>
+</div>
+</div>
+</div>
+</div>
+<section>
+    <div class="slider_img layout_two">
+        <div id="carousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel" data-slide-to="1"></li>
+                <li data-target="#carousel" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <img class="d-block" src="images/slide4.jpg" alt="First slide">
+                    <div class="carousel-caption d-md-block">
+                        <div class="slider_title">
+                            <h1>Welcome To The<br>SHRIJI BABA SARASWATI VIDYA MANDIR</h1>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block" src="images/slide2.jpg" alt="Second slide">
+                    <div class="carousel-caption d-md-block">
+                        <div class="slider_title">
+                            <h1></h1>
+                            <h4></h4>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block" src="images/slide3.jpg" alt="Third slide">
+                    <div class="carousel-caption d-md-block">
+                        <div class="slider_title">
+                            <h1>Campus life @ SJBSVM</h1>
+                            <h4></h4><br><br><br><br><br><br>
+                            <div class="slider-btn">
+                                <a href="campus-life.html" class="btn btn-default">Campus Life</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+                <i class="icon-arrow-left fa-slider" aria-hidden="true"></i>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+                <i class="icon-arrow-right fa-slider" aria-hidden="true"></i>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+</section>
+<!--//END HEADER -->
+<!--============================= ABOUT =============================-->
+<section class="clearfix about">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Welcome</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+               <p><strong>Shriji Baba Saraswati Vidya Mandir</strong> is located in Mathura, Uttar Pradesh, India. Considered among the top rung of Indian Public Schools, Shriji Baba Saraswati Vidya Mandir, Mathura popularly known as SJBSVM Mathura, is a Boarding-cum-Day school for Boys founded in 1985. Over the last three decades, <strong>SJBSVM</strong> has grown to a student strength of 1100 and staff strength of 100. The school prepares students for the AISSE (Class X) and the AISSCE (Class XII) Examinations conducted by the Central Board of Secondary Education, New Delhi.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!--//END ABOUT -->
+<!--============================= OUR COURSES =============================-->
+<section class="our_courses">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+               <center><h2>Campus life @ SJBSVM</h2></center><br>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+                <div class="courses_box mb-4">
+                    <div class="course-img-wrap">
+                        <img src="images/class.jpg" class="img-fluid" alt="courses-img">
+                    </div>
+                    <!-- // end .course-img-wrap -->
+                    <div class="courses_icon">
+                        <img src="images/plus-icon.png" class="img-fluid close-icon" alt="plus-icon">
+                    </div>
+                    <a href="lab.php" class="course-box-content">
+                        <h3>Class Rooms</h3>
+                        <p>The classrooms are spacious, well-lighted...</p>
+                    </a>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+                <div class="courses_box mb-4">
+                    <div class="course-img-wrap">
+                        <img src="images/computerlab.jpg" class="img-fluid" alt="courses-img">
+                    </div>
+                    <!-- // end .course-img-wrap -->
+                    <div class="courses_icon">
+                        <img src="images/plus-icon.png" class="img-fluid close-icon" alt="plus-icon">
+                    </div>
+                    <a href="lab.php" class="course-box-content">
+                        <h3>Computer Lab</h3>
+                        <p>The School has two computer labs...</p>
+                    </a>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+                <div class="courses_box mb-4">
+                    <div class="course-img-wrap">
+                        <img src="images/labo.jpg" class="img-fluid" alt="courses-img">
+                    </div>
+                    <!-- // end .course-img-wrap -->
+                    <div class="courses_icon">
+                        <img src="images/plus-icon.png" class="img-fluid close-icon" alt="plus-icon">
+                    </div>
+                    <a href="lab.php" class="course-box-content">
+                        <h3>Laboratories</h3>
+                        <p>The school has well set up science laboratories...</p>
+                    </a>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+                <div class="courses_box mb-4">
+                    <div class="course-img-wrap">
+                        <img src="images/sport.jpeg" class="img-fluid" alt="courses-img">
+                    </div>
+                    <!-- // end .course-img-wrap -->
+                    <div class="courses_icon">
+                        <img src="images/plus-icon.png" class="img-fluid close-icon" alt="plus-icon">
+                    </div>
+                    <a href="lab.php" class="course-box-content">
+                        <h3>Sports </h3>
+                        <p>The school has been in the forefront...</p>
+                    </a>
+                </div>
+            </div>
+        </div> <br>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <a href="#" class="btn btn-default btn-courses">View more Activity</a>
+            </div>
+        </div>
+    </div>
+</section>
+<!--//END OUR COURSES -->
+<!--============================= EVENTS =============================-->
+<section class="event">
+
+    <center><h2><u>News and Events</u></h2></center><br>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6"> 
+                <div class="event-img2">
+
+                <div class="row">
+                    <div class="col-sm-3"> <img src="images/upcoming-event-img.jpg" class="img-fluid" alt="event-img"></div><!-- // end .col-sm-3 -->
+                    <div class="col-sm-9"> <h3>Entrance Exam Dates</h3>
+                    <span>25-Feb-18</span>
+                    <p>Entrance Exam reporting time will be announced.</p>
+                    <a href="#">Read More</a></div><!-- // end .col-sm-7 -->
+                </div><!-- // end .row -->
+                   
+                         <div class="row mt-4">
+                    <div class="col-sm-3"> <img src="images/upcoming-event-img.jpg" class="img-fluid" alt="event-img"></div><!-- // end .col-sm-3 -->
+                    <div class="col-sm-9"> <h3>Annual Function  </h3>
+                    <span>xx-xxx-xx</span>
+                    <p>.</p>
+                    <a href="#">Read More</a></div><!-- // end .col-sm-7 -->
+                </div><!-- // end .row -->                   
+                   
+                </div>
+            </div>
+            <div class="col-lg-6"> 
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="event_date">
+                            <div class="event-date-wrap">
+                                <p>25</p>
+                                <span>Feb.18</span>
+                            </div>
+                        </div>
+                        <div class="date-description">
+                            <h3>Result will be announced on website.</h3>
+                            <p>Result will be announced on website.</p>
+                            <hr class="event_line">
+                        </div>
+                        <div class="event_date">
+                            <div class="event-date-wrap">
+                                <p>25</p>
+                                <span>Feb.18</span>
+                            </div>
+                        </div>
+                        <div class="date-description">
+                            <h3>Result will be announced on website.</h3>
+                            <p>Result will be announced on website.</p>
+                            <hr class="event_line">
+                        </div>
+                    </div>
+                </div> 
+ 
+            </div>
+        </div>
+    </div>
+</section>
+<!--//END EVENTS -->
+<!--============================= FACT CHART =============================-->
+<div class="detailed_chart">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-3 chart_bottom">
+                <div class="chart-img">
+                    <img src="images/chart-icon_1.png" class="img-fluid" alt="chart_icon">
+                </div>
+                <div class="chart-text">
+                    <p><span class="counter">50</span> Teachers
+                    </p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3 chart_bottom chart_top">
+                <div class="chart-img">
+                    <img src="images/chart-icon_2.png" class="img-fluid" alt="chart_icon">
+                </div>
+                <div class="chart-text">
+                    <p><span class="counter">1500</span> Students
+                    </p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3 chart_top">
+                <div class="chart-img">
+                    <img src="images/chart-icon_3.png" class="img-fluid" alt="chart_icon">
+                </div>
+                <div class="chart-text">
+                    <p><span class="counter">5000</span> Books
+                    </p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <div class="chart-img">
+                    <img src="images/chart-icon_4.png" class="img-fluid" alt="chart_icon">
+                </div>
+                <div class="chart-text">
+                    <p><span class="counter">34</span> Years Exp.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--//END FACT CHART -->
+<!--============================= FOOTER =============================-->
+    <?php
+    include 'inc/footer.html';
+    ?>
+        <!--//END FOOTER -->
+        <!-- jQuery, Bootstrap JS. -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/tether.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Plugins -->
+        <script src="js/slick.min.js"></script>
+        <script src="js/waypoints.min.js"></script>
+        <script src="js/counterup.min.js"></script>
+        <script src="js/instafeed.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/validate.js"></script>
+        <script src="js/tweetie.min.js"></script>
+        <!-- Subscribe -->
+        <script src="js/subscribe.js"></script>
+        <!-- Script JS -->
+        <script src="js/script.js"></script>
+    </body>
+
+    </html>
