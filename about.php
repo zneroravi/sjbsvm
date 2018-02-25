e0c4089ee23dcc43e3252d0a487c59290494dc07
Ravi Goswami <zneroravi@gmail.com>
2018-02-25 13:05:24 +0530

    up3

---
+++ about.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>About</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <!-- Simple Line Font -->
+    <link rel="stylesheet" href="css/simple-line-icons.css">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/slick-theme.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <!-- Main CSS -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- ========================= ABOUT IMAGE =========================-->
+    <?php
+    include 'inc/menu2.html';
+    ?>
+    
+        <div class="row">
+            <div class="col-md-12">
+                <h1>About Us</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<!--//END ABOUT IMAGE -->
+<!--============================= ABOUT TITLE =============================-->
+<section class="welcome_about">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>About SJBSVM</h2>
+                <p>Shriji Baba Saraswati Vidya Mandir located in Mathura , is established with a vision of providing quality education to students. The school is a Boys school affiliated to Central Board of Secondary Education (CBSE) . Shriji Baba Saraswati Vidya Mandir is dedicated to provide world class educational facilities to the students and offers education from classes VI - XII. To ensure the complete development of the children, Shriji Baba Saraswati Vidya Mandir encourages the students to take part in various co-curricular activities held in school such as music, dance, arts and sports. The campus of Shriji Baba Saraswati Vidya Mandir is well equipped with the modern infrastructural facilities such as spacious ventilated class rooms with the latest teaching aids, rich library, state-of-the-art computer labs, science labs and play grounds.
+                    <br>
+                    <br> </p>
+                </div>
+                <div class="col-md-5"><br><br><br>
+                    <img src="images/sjbsvm.jpg" class="img-fluid" alt="#">
+                </div>
+                <p>Shriji Baba Saraswati Vidya Mandir is located in Mathura, Uttar Pradesh, India. Considered among the top rung of Indian Public Schools, Shriji Baba Saraswati Vidya Mandir, Mathura popularly known as SJBSVM Mathura, is a Boarding-cum-Day school for Boys founded in 1985. Over the last three decades, SJBSVM has grown to a student strength of 1100 and staff strength of 100. The school prepares students for the AISSE (Class X) and the AISSCE (Class XII) Examinations conducted by the Central Board of Secondary Education, New Delhi.</p>
+            </div>
+        </div>
+    </section>
+    <!--//END ABOUT TITLE -->
+
+    <!--============================= OUR TEACHERS =============================-->
+    <section class="testimonial">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Our Significance</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-6">
+                    <div class="our-teachers-block">
+                        <img src="images/ajay.png" class="img-fluid teachers-img" alt="#">
+                        <div class="teachers-description">
+                            <p><strong>Dr. Ajay Sharma</strong>
+                                <br> Principal
+                            </p>
+                            <hr />
+                            <p>M.A.(Hindi & Military Science), B.Ed., Ph.D.</p>
+                            <div class="social-icons">
+                                <ul>
+                                    <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-6">
+                    <div class="our-teachers-block">
+                        <img src="images/pradeep.jpg" class="img-fluid teachers-img" alt="#">
+                        <div class="teachers-description">
+                            <p><strong>Mr. Pradeep Kumar</strong>
+                                <br> Vice-Principal
+                            </p>
+                            <hr />
+                            <p> P.G.T.,HoD(Maths),M.Sc.(Maths),B.Ed.</p>
+                            <div class="social-icons">
+                                <ul>
+                                    <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                <!-- End row -->
+            </div>
+        </section>
+        <!--//END OUR TEACHERS -->
+        <!--============================= OUR VISION ===========================-->
+        <section class="admission-form_rules">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-7 admission-form_mr">
+                        <h2>Our Aims & Objectives</h2>
+                        <p>The school aims at an all round development: physical, emotional and intellectual with consciousness towards social obligations, Indian art, culture, music, dance, yoga and languages.
+                        5 years of General Education is followed by 2 years of preparation leading to Engineering, Medicine, Commerce and Institutes of higher education and future careers.<br>
+                        The aim of the school is to impart an all-round education, so as to equip the students mentally, morally, physically and culturally to become worthy citizens of free India and enable them to zealously guard and retain its noble traditions. The School aims at disciplined students, educated in mind, body and spirit and hence best suited to serve their country and best fitted to lead others by their example.</p>
+                    </div>
+                    <div class="col-md-5 admission-form_mr">
+                        <ul class="admission-form_listed">
+                            <li>To create an atmosphere in which each child feels happy, secure, confident & valued.</li>
+                            <li>To provide broad and balanced curriculum appropriate to the social, emotional, spiritual, and intellectual development of Children.</li>
+                            <li>To provide learning experiences which are first hand and active as far as possible.</li>
+                            <li>To foster qualities of tolerance, kindness and respect for others and good manners.</li>
+                            <li>To aim for excellence in all activities & experience.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--//END VISION RULES -->
+
+    
+        <!--============================= FOOTER =============================-->
+        <?php
+        include 'inc/footer.html';
+        ?>
+                <!--//END FOOTER -->
+                <!-- jQuery, Bootstrap JS. -->
+                <script src="js/jquery.min.js"></script>
+                <script src="js/tether.min.js"></script>
+                <script src="js/bootstrap.min.js"></script>
+                <!-- Plugins -->
+                <script src="js/slick.min.js"></script>
+                <script src="js/waypoints.min.js"></script>
+                <script src="js/counterup.min.js"></script>
+                <script src="js/instafeed.min.js"></script>
+                <script src="js/owl.carousel.min.js"></script>
+                <script src="js/validate.js"></script>
+                <script src="js/tweetie.min.js"></script>
+                <!-- Subscribe -->
+                <script src="js/subscribe.js"></script>
+                <!-- Script JS -->
+                <script src="js/script.js"></script>
+            </body>
+
+            </html>
